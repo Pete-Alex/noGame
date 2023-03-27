03c3b5b25b4c9d7c3ee03d644fd85883e45d3a79
ABenat1988 <ale.benat@gmail.com>
2023-03-27 16:21:40 +0200

    push

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,31 @@
 const express = require('express');
+
+
+const BuildingType = require('../models/BuildingType.model');
+const Planet = require('../models/Planet.model');
+const User = require('../models/User.model');
+
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+
+router.get('/planet/:planetId', (req, res, next) => {
+  const planetId = req.params.planetId;
+
+  Planet.findById(planetId)
+    .populate("buildings.BuildingTypeId")
+    .then((response) => {     
+      console.log(response);
+      res.render("planet-detail", response);
+    })
+    .catch(e => {
+      console.log("error getting the planet", e);
+      next(e);
+    });
+})
 
 module.exports = router;
